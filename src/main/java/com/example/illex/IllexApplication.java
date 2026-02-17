601e89e162939a141ac2ac281f6008f92b873e13
Antonio M.D.V <antonio.61conexaoxxi@gmail.com>
2026-02-17 16:23:09 -0300

    Git setup & basic config

---
+++ src/main/java/com/example/illex/IllexApplication.java
@@ -0,0 +1,13 @@
+package com.example.illex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IllexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IllexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/illex/IllexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.illex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IllexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
