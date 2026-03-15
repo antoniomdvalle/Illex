7b48c4580bf631ddbc2c35bdbf4bb13c67f40704
Antonio M.D.V <antonio.61conexaoxxi@gmail.com>
2026-03-15 20:40:54 -0300

    Authentication debug. Since now, the full register-login REST API is working.

--- src/main/java/com/example/illex/config/SecurityConfig.java
+++ src/main/java/com/example/illex/config/SecurityConfig.java
@@ -2,12 +2,30 @@ package com.example.illex.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll()
+                .anyRequest().permitAll()
+            )
+            .httpBasic(basic -> basic.disable())
+            .formLogin(form -> form.disable());
+
+        return http.build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
